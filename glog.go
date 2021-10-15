bf4f5cfcc090bbeef702e3cb22181812b1c18429
xuechenghan(韩学成) <xuechenghan@tencent.com>
2021-10-15 20:05:34 +0800

    fix bug

--- glog.go
+++ glog.go
@@ -398,7 +398,7 @@ type flushSyncWriter interface {
 func init() {
 	flag.BoolVar(&logging.toStderr, "logToStdErr", false, "log to standard error instead of files")
 	flag.BoolVar(&logging.alsoToStderr, "alsoLogToStdErr", false, "log to standard error as well as files")
-	flag.Var(&logging.verbosity, "v", "log level for V logs")
+	flag.Var(&logging.verbosity, "ver", "log level for V logs")
 	flag.Var(&logging.stderrThreshold, "stdErrThreshold", "logs at or above this threshold go to stderr")
 	flag.Var(&logging.vmodule, "vModule", "comma-separated list of pattern=N settings for file-filtered logging")
 	flag.Var(&logging.traceLocation, "logBackTraceAt", "when logging hits line file:N, emit a stack trace")
